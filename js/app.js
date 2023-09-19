f7b3ab438e682044f849c49d62a46d4f1f4cccc6
Afzal Momin <afzalmomin2003@gmail.com>
2023-09-20 00:49:01 +0530

    Added Dynamic Calorie stats

--- js/app.js
+++ js/app.js
@@ -5,15 +5,54 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        this._dislpayCalorieLimit();
+        this._displayCalorieConsumed();
     }
-    // public methods
+    // public methods // 
     addMeal(meal){
         this._meals.push(meal);
         this._totalCalories += meal.calories;
+        this._render();
     }
     addWorkouts(workout){
         this._workouts.push(workout);
         this._totalCalories -= workout.calories;
+        this._render();
+    }
+    // Private methods //
+    _dislpayCalorieLimit () {   // dynamic calorie limit diplayer
+        const CalorieLimit = document.querySelector('#calories-limit');
+        CalorieLimit.innerHTML = this._calorieLimit;
+    }
+
+    _displayTotalCalories () {   // dynamic total calories diplayer
+        const TotalCalorieEl = document.querySelector('#calories-total')
+        TotalCalorieEl.innerHTML = this._totalCalories;
+    }
+
+    _displayCalorieConsumed () { // dynamic calorie consumed diplayer
+        const consumed = this._meals.reduce((total,meals) => total + meals.calories, 0);
+        const calorieConsumed = document.querySelector('#calories-consumed');
+        calorieConsumed.innerHTML = consumed;
+    }
+
+    _displayCalorieBurned () {  // dynamic calorie burned diplayer
+        const burned = this._workouts.reduce((total,workout) => total + workout.calories, 0);
+        const calorieburned = document.querySelector('#calories-burned');
+        calorieburned.innerHTML = burned;
+    }
+
+    _displaycaloriesRemaining () {  // dynamic calorie remaning diplayer
+        const calRemaining = this._calorieLimit - this._totalCalories;
+        const caloriesRemaining = document.querySelector('#calories-remaining');
+        caloriesRemaining.innerHTML = calRemaining;
+    }  
+
+    _render() {
+        this._displayTotalCalories();
+        this._displayCalorieConsumed();
+        this._displayCalorieBurned();
+        this._displaycaloriesRemaining();
     }
 }
 
@@ -22,6 +61,7 @@ class Meal {
         this.id = Math.random().toString(16).slice(2);
         this.name = name;
         this.calories = calories;
+        
     }
 }
 class Workouts{
@@ -29,13 +69,13 @@ class Workouts{
         this.id = Math.random().toString(16).slice(2);
         this.name = name;
         this.calories = calories;
+        
     }
 }
 
 const tracker = new CalorieTracker();
-const breakfast = new Meal('Bread Jam', 1000);
-const lunch = new Meal('Rice & Curry', 1000);
-const workout = new Workouts('Morning run',500 );
+const breakfast = new Meal('Bread Jam', 400);
+const workout = new Workouts('Morning run',320);
 tracker.addMeal(breakfast);
 tracker.addWorkouts(workout);
 console.log(tracker._totalCalories);
