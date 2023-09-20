00cf2ae8fe21ae9465cf3f1012eb008276e26b28
Afzal Momin <afzalmomin2003@gmail.com>
2023-09-20 12:44:38 +0530

    Added color transition for calories-consumed & progress-bar from success/green to danger/red

--- js/app.js
+++ js/app.js
@@ -45,14 +45,36 @@ class CalorieTracker {
     _displaycaloriesRemaining () {  // dynamic calorie remaning diplayer
         const calRemaining = this._calorieLimit - this._totalCalories;
         const caloriesRemaining = document.querySelector('#calories-remaining');
+        const progressEl = document.querySelector('#calorie-progress');
         caloriesRemaining.innerHTML = calRemaining;
+        // color change of parent card
+        if (calRemaining <= 0){
+            caloriesRemaining.parentElement.parentElement.classList.remove('bg-light');
+            caloriesRemaining.parentElement.parentElement.classList.add('bg-danger');
+            progressEl.classList.remove('bg-success');
+            progressEl.classList.add('bg-danger');
+
+        }else{
+            caloriesRemaining.parentElement.parentElement.classList.add('bg-light');
+            caloriesRemaining.parentElement.parentElement.classList.remove('bg-danger');
+            progressEl.classList.add('bg-success');
+            progressEl.classList.remove('bg-danger');
+        }
     }  
 
+    _displayProgressBar () {
+        const progressEl = document.querySelector('#calorie-progress');
+        const progress = ((this._totalCalories / this._calorieLimit) * 100);
+        const progressPercent = Math.min(progress,100);
+        progressEl.style.width = `${progressPercent}%`;
+
+    }
     _render() {
         this._displayTotalCalories();
         this._displayCalorieConsumed();
         this._displayCalorieBurned();
         this._displaycaloriesRemaining();
+        this._displayProgressBar();
     }
 }
 
@@ -74,8 +96,9 @@ class Workouts{
 }
 
 const tracker = new CalorieTracker();
-const breakfast = new Meal('Bread Jam', 400);
+const breakfast = new Meal('Bread Jam', 4000);
+const lunch = new Meal('lunch',600)
 const workout = new Workouts('Morning run',320);
 tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
 tracker.addWorkouts(workout);
-console.log(tracker._totalCalories);
